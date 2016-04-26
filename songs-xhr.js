2fd5727da7ef02b35aa6657525762ce11ec932d4
ZachParris <zacharyjparris@gmail.com>
2016-04-25 19:43:14 -0500

    finished MH5

---
+++ songs-xhr.js
@@ -0,0 +1,30 @@
+// "use strict"
+
+
+// 	var songs = [];
+
+
+//     function getSongs() {
+//       		return songs;
+//    }
+//     function loadSongList () {
+//       var loader = new XMLHttpRequest();
+
+//         loader.addEventListener("load", loadJson);
+
+//         loader.open("GET", "songList.json");
+//         loader.send();
+
+//         function loadJson() {
+//         songs = JSON.parse(this.responseText).songs;
+      
+    
+//         };
+//         }
+    
+
+
+
+
+
+
